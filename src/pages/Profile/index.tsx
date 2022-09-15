e933784bd98db406afb86c0af2222dfb083cc6bb
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-15 18:57:54 -0300

    criação do index das telas

---
+++ src/pages/Profile/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+
+export default function Profile() {
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
