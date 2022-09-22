66571aae9b58d2db0ade8f229ceaadddcf4b423d
rgoudinho <ronaldogoudinho815@gmail.com>
2022-09-22 19:31:44 -0300

    adding test routes for plants

--- src/pages/ListPlant/index.tsx
+++ src/pages/ListPlant/index.tsx
@@ -1,11 +1,15 @@
-import { View, Text, StyleSheet } from "react-native";
+import { View, Button, Text, StyleSheet } from "react-native";
 import React from "react";
 
-export default function ListPlant() {
+export default function ListPlant({navigation}) {
+  const press = () => {
+    navigation.navigate("Plant");
+  };
+
   return (
     <View>
       <Text>
-        
+      <Button title="press" onPress={press} />
       </Text>
     </View>
   );
