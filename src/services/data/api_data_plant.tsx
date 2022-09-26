92d8f64443d0f40c82a7ba50438e4f7ce15cde33
Andres <andres.porfirio@gmail.com>
2022-09-26 20:41:45 -0300

    fixed api_data_plant export

--- src/services/data/api_data_plant.tsx
+++
@@ -1,5 +0,0 @@
-const data_plant = [
-    {id: 1, name: "Ortelã-pimenta", scientificName: "Mentha piperita", description: "A hortelã-pimenta é uma hortelã híbrida obtida do cruzamento entre Mentha aquatica e Mentha spicata." , img: "../../assets/images/plant/1.webp", problemsDealtWith: ["gripe", "resfriado", "dor de cabeça"], },
-    {id: 2, name: "Erva-cidreira", scientificName: "Melissa officinalis", description: "A erva-cidreira, também conhecida popularmente como erva-cidreira verdadeira, ou apenas por melissa, é uma planta perene herbácea da família da menta /hortelã e do boldo, nativa da Europa meridional" , img: "../../assets/images/plant/2.jpg", problemsDealtWith: ["Herpes labial", "TPM", "dor de cabeça"], },
-    {id: 3, name: "Lúcia-lima", scientificName: "Aloysia citrodora", description: "Lúcia-lima é uma espécie angiospérmica da família das verbenáceas, nativa da América do Sul. Foi introduzida na Europa pelos espanhóis e portugueses no século XVII." , img: "../../assets/images/plant/3.webp", problemsDealtWith: ["Insônia", "Febre", "Asma"], },
-]
